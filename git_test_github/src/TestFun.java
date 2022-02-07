4fe997a84801fee1794178d7040170e8912d8430
guofy <18334680806@163.com>
2022-02-07 19:05:09 +0800

    第一次推送到github中去

---
+++ git_test_github/src/TestFun.java
@@ -0,0 +1,13 @@
+public class TestFun {
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+		System.out.println("hello world!");
+		System.out.println("hello world!");
+		System.out.println("hello world!");
+
+	}
+	public void test1(){
+		int i = 10;
+		i += 10;
+	}
+}

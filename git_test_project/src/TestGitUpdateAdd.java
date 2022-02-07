8f06db71d442a6028e34c6bbe202a54478ba266e
guofy <18334680806@163.com>
2022-02-07 18:14:30 +0800

    第一次往GIt本地仓库中添加文件

---
+++ git_test_project/src/TestGitUpdateAdd.java
@@ -0,0 +1,10 @@
+public class TestGitUpdateAdd {
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+		System.out.println("hello world!");
+		System.out.println("hello world!");
+		System.out.println("hello world!");
+		System.out.println("hello world!");
+
+	}
+}
